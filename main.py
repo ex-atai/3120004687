459036773bce202dfcdd88a7ec4af345d9fb65c1
ex-atai <112877668+ex-atai@users.noreply.github.com>
2022-09-22 16:38:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 # This is a sample Python script.
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-# This is a sample Python script.
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-import re
+
 import sys
-import os
+
 
 def path_check(path):
   str=' '
